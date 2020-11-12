f5147d0ac116b2f5e3e8276261d8600d2958f78c
kjyoo <>
2020-11-12 10:26:09 +0900

    rx java

--- mylibrary/src/main/java/com/giosis/library/BaseViewModel.kt
+++ mylibrary/src/main/java/com/giosis/library/BaseViewModel.kt
@@ -1,8 +1,20 @@
 package com.giosis.library
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 
-open class BaseViewModel : ViewModel() {
+open class BaseViewModel() : ViewModel() {
 
+    private val disposables = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
+        super.onCleared()
+    }
 
 }
