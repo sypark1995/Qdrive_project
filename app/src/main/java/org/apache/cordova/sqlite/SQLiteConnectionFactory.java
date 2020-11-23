fc0ac52a18d27f5e3a2440be2bea651a0e93b27d
kjyoo <>
2020-11-23 16:53:29 +0900

    코도바 삭제

--- app/src/main/java/org/apache/cordova/sqlite/SQLiteConnectionFactory.java
+++
@@ -1,5 +0,0 @@
-package org.apache.cordova.sqlite;
-
-public interface SQLiteConnectionFactory {
-  public SQLiteConnection newSQLiteConnection(String filename, int flags) throws java.sql.SQLException;
-}
