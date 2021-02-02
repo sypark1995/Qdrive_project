8dfdc822934e5aea447d80844a5c90ccdf077b76
kjyoo <>
2021-02-02 16:27:36 +0900

    소스 정리

--- appMY/src/main/java/com/giosis/util/qdrive/international/ManualSelfCollectorHelper.java
+++ appMY/src/main/java/com/giosis/util/qdrive/international/ManualSelfCollectorHelper.java
@@ -14,9 +14,9 @@ import com.giosis.library.list.BarcodeData;
 import com.giosis.library.list.SigningView;
 import com.giosis.library.server.Custom_JsonParser;
 import com.giosis.library.server.ImageUpload;
+import com.giosis.library.util.BarcodeType;
 import com.giosis.library.util.DisplayUtil;
 import com.giosis.library.util.NetworkUtil;
-import com.giosis.util.qdrive.util.BarcodeType;
 import com.giosis.util.qdrive.util.DataUtil;
 
 import org.json.JSONObject;
