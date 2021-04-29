043a15cd7df32b2bbb5cb426b44b5f70e77633bb
kjyoo <>
2021-04-29 16:49:55 +0900

    merge 작업

--- appMY/src/main/java/com/giosis/util/qdrive/international/LoginActivity.kt
+++ appMY/src/main/java/com/giosis/util/qdrive/international/LoginActivity.kt
@@ -26,7 +26,6 @@ import com.giosis.library.util.NetworkUtil
 import com.giosis.library.util.PermissionActivity
 import com.giosis.library.util.PermissionChecker
 import com.giosis.util.qdrive.util.*
-import com.giosis.util.qdrive.util.ui.CommonActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 import java.io.File
