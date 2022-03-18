5a2272be7a41e69599e743468fb35ed4c5cb98fb
kwangjaeyoo <yookj00@gmail.com>
2022-03-18 18:02:37 +0900

    reformat

--- mylibrary/src/main/java/com/giosis/library/barcodescanner/ScannedBarcodeAdapter.kt
+++ mylibrary/src/main/java/com/giosis/library/barcodescanner/ScannedBarcodeAdapter.kt
@@ -13,14 +13,16 @@ import com.giosis.library.list.BarcodeData
 import com.giosis.library.util.BarcodeType
 import java.util.*
 
-class ScannedBarcodeAdapter(var items: ArrayList<BarcodeData>?, private var scanType: String) : RecyclerView.Adapter<ScannedBarcodeAdapter.ViewHolder>() {
+class ScannedBarcodeAdapter(var items: ArrayList<BarcodeData>?, private var scanType: String) :
+    RecyclerView.Adapter<ScannedBarcodeAdapter.ViewHolder>() {
 
     private var mContext: Context? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         mContext = parent.context
-        val binding = ItemCaptureScannedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemCaptureScannedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -44,15 +46,26 @@ class ScannedBarcodeAdapter(var items: ArrayList<BarcodeData>?, private var scan
 
                 mContext?.let {
 
-                    holder.binding.textBarcode.setTextSize(TypedValue.COMPLEX_UNIT_PX, it.resources.getDimension(R.dimen.text_size_36px))
-                    holder.binding.textBarcode.setTextColor(ContextCompat.getColor(it, R.color.color_303030))
+                    holder.binding.textBarcode.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        it.resources.getDimension(R.dimen.text_size_36px)
+                    )
+                    holder.binding.textBarcode.setTextColor(
+                        ContextCompat.getColor(
+                            it,
+                            R.color.color_303030
+                        )
+                    )
                 }
 
                 if (scanType == BarcodeType.CHANGE_DELIVERY_DRIVER) {
 
                     mContext?.let {
 
-                        holder.binding.textBarcode.setTextSize(TypedValue.COMPLEX_UNIT_PX, it.resources.getDimension(R.dimen.text_size_24px))
+                        holder.binding.textBarcode.setTextSize(
+                            TypedValue.COMPLEX_UNIT_PX,
+                            it.resources.getDimension(R.dimen.text_size_24px)
+                        )
                     }
                     holder.binding.btnState.visibility = View.GONE
                 } else if (scanType == BarcodeType.OUTLET_PICKUP_SCAN) {
@@ -67,7 +80,12 @@ class ScannedBarcodeAdapter(var items: ArrayList<BarcodeData>?, private var scan
 
                 mContext?.let {
 
-                    holder.binding.textBarcode.setTextColor(ContextCompat.getColor(it, R.color.color_909090))
+                    holder.binding.textBarcode.setTextColor(
+                        ContextCompat.getColor(
+                            it,
+                            R.color.color_909090
+                        )
+                    )
                 }
 
                 if (scanType == BarcodeType.OUTLET_PICKUP_SCAN) {
@@ -82,14 +100,11 @@ class ScannedBarcodeAdapter(var items: ArrayList<BarcodeData>?, private var scan
     }
 
     override fun getItemCount(): Int {
-
         if (items != null) {
-
             if (0 < items!!.size) {
                 return items!!.size
             }
         }
-
         return 0
     }
 }
