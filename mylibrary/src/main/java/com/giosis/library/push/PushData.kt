2a29bd6f7fe7b514e4553931e5d84b1c36527792
kwangjaeyoo <yookj00@gmail.com>
2022-04-04 14:49:35 +0900

    코틀린 추가

---
+++ mylibrary/src/main/java/com/giosis/library/push/PushData.kt
@@ -0,0 +1,30 @@
+package com.giosis.library.push
+
+object PushData {
+    ////////////////////////////////////
+    const val TITLE = "title"
+    const val MESSAGE = "message"
+    const val ACTION_KEY = "action_key"
+    const val ACTION_VALUE = "action_value"
+
+
+    ////////////////////////////////////
+    const val PICKUP_CANCEL = "PX"
+    const val SevenEle_TAKEBACK = "7ETB" // 7E TakeBack
+    const val FL_TAKEBACK = "FLTB" // FL TakeBack
+    const val Locker_EXPIRED = "LAE" // Locker Alliance Expired
+
+    const val LZD_PICK = "LZD_PICK" // lazada pickup 건 처리
+
+    ////////////////////////////////////
+
+    const val DOWNLOAD = "download"
+
+
+    const val QX_MSG = "QXMSG"
+    const val QST = "QST"
+
+    const val LAE = "LAE"
+    const val SRL = "SRL"
+
+}
