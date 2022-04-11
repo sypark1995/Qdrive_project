905ea6fb011be9e86accde2dfc48c3d84f6d0965
kwangjaeyoo <yookj00@gmail.com>
2022-04-11 11:02:18 +0900

    주석 삭제

--- mylibrary/src/main/java/com/giosis/library/main/leftMenu/LeftChildViewAdapter.kt
+++ mylibrary/src/main/java/com/giosis/library/main/leftMenu/LeftChildViewAdapter.kt
@@ -34,59 +34,6 @@ class LeftChildViewAdapter(val item: ArrayList<SubMenuItem>) :
                 intent.putExtras(Bundle().apply(item.extras))
                 (view.context as MainActivity).startActivity(intent)
 
-//                when (item) {
-//                    view.resources.getString(R.string.text_start_delivery_for_outlet),
-//                    view.resources.getString(R.string.navi_sub_confirm_delivery) -> {
-//                        val intent = Intent(view.context, CaptureActivity1::class.java)
-//                        intent.putExtra(
-//                            "title",
-//                            view.context.resources.getString(R.string.text_title_driver_assign)
-//                        )
-//                        intent.putExtra("type", BarcodeType.CONFIRM_MY_DELIVERY_ORDER)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.navi_sub_delivery_done) -> {
-//                        val intent = Intent(view.context, CaptureActivity1::class.java)
-//                        intent.putExtra(
-//                            "title",
-//                            view.context.resources.getString(R.string.text_delivered)
-//                        )
-//                        intent.putExtra("type", BarcodeType.DELIVERY_DONE)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.navi_sub_pickup) -> {
-//                        val intent = Intent(view.context, CaptureActivity1::class.java)
-//                        intent.putExtra(
-//                            "title",
-//                            view.context.resources.getString(R.string.text_title_scan_pickup_cnr)
-//                        )
-//                        intent.putExtra("type", BarcodeType.PICKUP_CNR)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.navi_sub_self) -> {
-//                        val intent = Intent(view.context, CaptureActivity1::class.java)
-//                        intent.putExtra(
-//                            "title",
-//                            view.context.resources.getString(R.string.navi_sub_self)
-//                        )
-//                        intent.putExtra("type", BarcodeType.SELF_COLLECTION)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.navi_sub_in_progress),
-//                    view.resources.getString(R.string.navi_sub_upload_fail),
-//                    view.resources.getString(R.string.navi_sub_today_done) -> {
-//                        val intent = Intent(view.context, ListActivity::class.java)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.navi_sub_not_in_housed) -> {
-//                        val intent = Intent(view.context, ListNotInHousedActivity::class.java)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                    view.resources.getString(R.string.text_outlet_order_status) -> {
-//                        val intent = Intent(view.context, OutletOrderStatusActivity::class.java)
-//                        (view.context as AppBaseActivity).startActivity(intent)
-//                    }
-//                }
                 notifyDataSetChanged()
             }
         }
