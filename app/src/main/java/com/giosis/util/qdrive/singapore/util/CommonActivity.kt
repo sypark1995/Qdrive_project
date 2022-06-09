61506eceb185aee7c8550410519b09f2b2ae3972
kwangjaeyoo <yookj00@gmail.com>
2022-06-09 10:11:32 +0900

    소스 정리

--- app/src/main/java/com/giosis/util/qdrive/singapore/util/CommonActivity.kt
+++ app/src/main/java/com/giosis/util/qdrive/singapore/util/CommonActivity.kt
@@ -1,15 +1,7 @@
 package com.giosis.util.qdrive.singapore.util
 
 import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.giosis.util.qdrive.singapore.LoginActivity
-import com.giosis.util.qdrive.singapore.R
-import com.giosis.util.qdrive.singapore.list.pickup.CnRPickupDoneActivity
-
 
 /**
  * 공통으로 적용하기 위한 BaseActivity
